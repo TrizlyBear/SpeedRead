cf9241079681f71d9d91e5cad3b8410c6a72a00c
TrizlyBear <tristanschmidt2004@gmail.com>
2019-05-24 16:29:18 +0200

    Almost finished

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ const {app, BrowserWindow} = electron;
 let index;
 
 app.on('ready', function(){
-   index = new BrowserWindow({webPreferences: {
-        nodeIntegration: true
-       }});
+   index = new BrowserWindow({
+       webPreferences: {nodeIntegration: true},
+       width: 625,
+       height: 375,
+       title: "Speed Read",
+       minHeight: 325,
+       minWidth: 680,
+       frame: false,
+       transparent: true,});
        index.loadURL(url.format({
            pathname: path.join(__dirname, 'index/index.html'),
            protocol: 'file:',
